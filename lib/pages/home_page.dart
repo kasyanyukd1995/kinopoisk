55709a08df1df14224768408fcf67954d5679fd8
kasyanyukd1995 <kasyanyuk.d1995@gmail.com>
2020-08-04 07:28:00 +0300

    7

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
---
+++ lib/pages/launcher/launcher_page.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+
+import 'animated_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  Animation _arrowAnimation, _heartAnimation;
+  AnimationController _arrowAnimationController, _heartAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _arrowAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _arrowAnimation =
+        Tween(begin: 0.0, end: pi).animate(_arrowAnimationController);
+
+    _heartAnimationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1200));
+    _heartAnimation = Tween(begin: 150.0, end: 170.0).animate(CurvedAnimation(
+        curve: Curves.bounceOut, parent: _heartAnimationController));
+
+    _heartAnimationController.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        _heartAnimationController.repeat();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _arrowAnimationController?.dispose();
+    _heartAnimationController?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Example Animations'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          firstChild(),
+          SizedBox(
+            height: 50.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget firstChild() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        AnimatedBuilder(
+          animation: _arrowAnimationController,
+          builder: (context, child) => Transform.rotate(
+            angle: _arrowAnimation.value,
+            child: Icon(
+              Icons.expand_more,
+              size: 50.0,
+              color: Colors.black,
+            ),
+          ),
+          //  child:
+        ),
+
+        //  _arrowAnimationController.isCompleted
+        //      ? _arrowAnimationController.reverse()
+        //     : _arrowAnimationController.forward();
+        //
+      ],
+    );
+  }
+}
